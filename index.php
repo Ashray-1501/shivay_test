1c3279c071aae85a5da9bbd8d143b4229386932c
SilentDevil-0912 <109501782+SilentDevil-0912@users.noreply.github.com>
2022-07-27 11:17:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
+<!DOCTYPE html>
 <html>
-   
-   <head>
-      <title>Hello World</title>
-   </head>
-   
-   <body>
-      <?php echo "Hello, World!";?>
-   </body>
+<body>
 
+<?php
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+?>
+
+</body>
 </html>
